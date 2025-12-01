09e40548b6c30ec021e39f1aa0baed8d3c1c6aea
krspv <31394089+krspv@users.noreply.github.com>
2025-12-01 19:18:50 +0100

    Day 1

---
+++ Day1 - Secret Entrance/Program.cs
@@ -0,0 +1,69 @@
+﻿// Day 12 - Garden Groups
+using Helpers;
+using System.Diagnostics;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Stopwatch stopwatch = new();
+PrintHelper.ПечатиНаслов(1, "This is a title");
+
+string fileName = Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName, "input.txt");
+
+
+
+
+
+// Read the input
+List<int> numbers = [.. File.ReadAllLines(fileName).Select(line => Int32.Parse(line[1..]) * (line[0] == 'L' ? -1 : 1))];
+
+
+
+
+stopwatch.Start();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Part 1
+int nPos = 50, nCount = 0;
+numbers.ForEach(number => {
+  nPos += number;
+  while (Math.Abs(nPos) > 99)
+    nPos -= 100 * Math.Sign(nPos);
+  if (nPos == 0)
+    nCount++;
+});
+// Part 1
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+stopwatch.Stop();
+
+PrintHelper.ПечатиПрвДел(stopwatch.ElapsedMilliseconds, nCount);
+
+
+
+
+
+stopwatch.Restart();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Part 2
+nPos = 50;
+nCount = 0;
+numbers.ForEach(number => {
+  int dir = number > 0 ? 1 : -1;
+  for (int x = 0; x < Math.Abs(number); ++x) {
+    nPos += dir;
+    if (nPos == 100)
+      nPos = 0;
+    else if (nPos == -1)
+      nPos = 99;
+    if (nPos == 0)
+      nCount++;
+  }
+});
+// Part 2
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+stopwatch.Stop();
+
+PrintHelper.ПечатиВторДел(stopwatch.ElapsedMilliseconds, nCount);
+
+
+
+
+
+PrintHelper.ПечатиЕлкаЗаКрај();
